6a51aa1b81d95b5415fa60e5e0229bc9b39786d4
Burburp <greasyhelio@proton.me>
2026-06-24 18:38:48 -0400

    Added Try Catch error handling practice

---
+++ Week4/Practice/try_practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  try {
+    std::vector<int> v = {1, 2, 3};
+    std::cout << v.at(10);
+  } catch (const std::out_of_range& e) {
+    std::cout << "Invalid index!";
+  }
+  return 0;
+}
